417ab100d65be124ab463f531b92702633337316
Humberto Sandmann <hsandmann@ieee.org>
2023-12-30 09:48:27 -0300

    just sthetic

--- src/main/java/store/gateway/GatewayDiscoveryConfiguration.java
+++ src/main/java/store/gateway/GatewayDiscoveryConfiguration.java
@@ -3,7 +3,7 @@ package store.gateway;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-    import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GatewayDiscoveryConfiguration {
