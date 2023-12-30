9ec32028e17b330b4d2dae12cd2a798f09a40952
Humberto Sandmann <hsandmann@ieee.org>
2023-12-30 09:49:25 -0300

    fixed names

--- src/main/java/store/gateway/GatewayConfiguration.java
+++ src/main/java/store/gateway/GatewayConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class GatewayDiscoveryConfiguration {
+public class GatewayConfiguration {
 
     @Bean
     @LoadBalanced
